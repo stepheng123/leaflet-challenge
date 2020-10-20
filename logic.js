ef169608de7d4304f27a25a64ec607dd337c03a9
Stephen Guilmartin <65271593+stepheng123@users.noreply.github.com>
2020-10-20 22:13:30 +0800

    final js

--- logic.js
+++ logic.js
@@ -1,30 +1,5 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
-
-// Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-});
-
-function createFeatures(earthquakeData) {
-console.log(earthquakeData);
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-  // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
-}
+// Perform API call to retrieve USGS earthquake data. Call createMarkers when complete.
+var geoData = d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson", createMarkers)
 
 function createMap(earthquakes) {
 
@@ -58,9 +33,7 @@ function createMap(earthquakes) {
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
   var myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
+    center: [37.09, -95.71],
     zoom: 5,
     layers: [streetmap, earthquakes]
   });
@@ -71,4 +44,84 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+
+// Create a legend to display map information
+var legend = L.control({position: "bottomleft"});
+
+ // When the layer control is added, insert a div with the class of "legend"
+ legend.onAdd = function() {
+    var div = L.DomUtil.create('div', 'info legend');
+    var labels = ["0-1", "1-2", "2-3", "3-4", "4-5", "5+"];;
+          
+    for (var i = 0; i < labels.length; i++) {
+      div.innerHTML += '<i style="background:' + getColor(i) + '"></i> ' +
+              labels[i] + '<br>' ;
+    }
+      return div;
+  };
+  
+  // Add the info legend to the map
+  legend.addTo(map);
 }
+
+
+ // Write function to create markers
+  function createMarkers(response) {
+  
+  // Select the features property from response
+  var features = response.features;
+  // console.log(features)
+
+  // Initialise an array to hold the magnitude and depth markers
+  var earthquakeMarkers = [];
+
+  // Loop through the features array
+  for (var index = 0; index < features.length; index++) {
+    var feature = features[index];
+    var location = feature.geometry;
+        
+    // For each earthquake, create a marker and bind a popup with additional information
+    var earthquakeMarker = L.circle([location.coordinates[1],location.coordinates[0]],{
+      radius:(feature.properties.mag)*20000,
+      fillColor: getColor(location.coordinates[2]),
+      fillOpacity: 0.8,
+      color: "black",
+      stroke: true,
+      weight: 0.5
+    })
+    .bindPopup("<h3>" + feature.properties.place + "<h3><h3>Magnitude: " + feature.properties.mag + "</h3>" + "<h3>Depth: " + location.coordinates[2] + "</h3>");
+  
+    // Add the marker to the earthquakeMarkers array
+    earthquakeMarkers.push(earthquakeMarker);
+  }
+
+  // Create a layer group made from the earthquakeMarkers array, pass it into the createMap function
+  createMap(L.layerGroup(earthquakeMarkers));
+  };
+
+// Write function to determine the fill colour of circle marker, based on depth
+
+function getColor(depth){
+  if (depth > 90) {
+    return "#FF3333";
+  }
+  else if (depth > 70) {
+    return "#FF6633";
+  }
+  else if (depth > 50) {
+    return "#FF9933";
+  }
+  else if (depth > 30) {
+    return "#FFCC33";
+  }
+  else if (depth > 10) {
+    return "#FFFF33";
+  }
+  else if ( depth > -10) {
+    return "#9CFF33";
+  }
+  else {
+    return "#000000";
+  }
+}
+
